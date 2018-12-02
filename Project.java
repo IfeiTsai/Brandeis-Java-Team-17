9febff6f028656a1cd999bba7b53f997d43982b3
Amanda La <amandala09@outlook.com>
2018-12-02 15:02:27 -0500

    Project.java
    prompts for user name

---
+++ Project.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Project{
+  public static void main(String [] args){
+    System.out.println("What is your name?");
+    Scanner sc = new Scanner(System.in);
+    String userName = sc.nextLine();
+  }
+}
