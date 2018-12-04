ba1f7eb34ec8a1d31d612b87a42e02de26a76730
colepeterson4 <44783601+colepeterson4@users.noreply.github.com>
2018-12-04 13:27:24 -0500

    Update Dates.java

--- Dates.java
+++ Dates.java
@@ -1,44 +1,89 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 /*major concerns: This will all dissapear once the program ends, we need to find a way to save information to a file
 and retrieve it. Additionally this currently can only save one line of input for the agenda at a time, and it's very
 vulnerable to improperly formatted/mistyped inputs from the user. More of a minimal viable product than an actual
-finished product. */
-public class Dates{
+finished project. */
+public class Dates2{
+  //These are all static arrays for each month containing a block for each day in the respective month.
+  static String[] jan = new String[31];
+  static String[] feb = new String[28];
+  static String[] mar = new String[31];
+  static String[] apr = new String[30];
+  static String[] may = new String[31];
+  static String[] jun = new String[30];
+  static String[] jul = new String[31];
+  static String[] aug = new String[31];
+  static String[] sep = new String[30];
+  static String[] oct = new String[31];
+  static String[] nov = new String[30];
+  static String[] dec = new String[31];
   public static void main(String[] args){
-//Here we create an array for each month with a space for each day.
-    String[] jan = new String[31];
-    String[] feb = new String[28];
-    String[] mar = new String[31];
-    String[] apr = new String[30];
-    String[] may = new String[31];
-    String[] jun = new String[30];
-    String[] jul = new String[31];
-    String[] aug = new String[31];
-    String[] sep = new String[30];
-    String[] oct = new String[31];
-    String[] nov = new String[30];
-    String[] dec = new String[31];
-
-//Next we place all of these arrays into a new array so that the correct date can be edited in one step
-//Not sure if this is actually helpful, feel free to tweak it.
-    String[][] dates = {jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec};
-//This is my code for putting a note or an agenda on the calendar for a given day.
-    System.out.println("To add an agenda please enter a date with the format: number of month [space] day. ");
-    System.out.println("For example March 24th should be written 3 24");
+    System.out.println("Welcome to the MY CALENDAR app!");
+//This while loop makes it so they can continue checking and editing the calendar until they choose to exit the program.
+    int exit = 0; // if this variable becomes 1 then the loop will end and the program will terminate.
+    while(exit !=1){
+//This is the main menu which allows the user to check their agenda for a given day, or add an agenda to another day.
+      System.out.printf("%n%nMAIN MENU:%n Please enter the number which corresponds to your desired option.%n");
+      System.out.printf("1 add an agenda for a day%n2 check an agenda for a day%n3 exit program%n%n");
+      Scanner console = new Scanner(System.in);
+      int option = console.nextInt();
+      switch(option){
+        case 1: addAgenda(); break;
+        case 2: checkAgenda(); break;
+        case 3: System.out.println("goodbye"); exit = 1; break;
+      }
+    }
+  }
+  public static void checkAgenda(){
+    System.out.printf("Please enter the date you would like to access with the format: first three letters of month [space] day.%n");
     Scanner console = new Scanner(System.in);
-    int month = console.nextInt()-1;
+    String month = console.next();
+    int day = console.nextInt()-1;
+    System.out.printf("%nGreat! your agenda for %s %d is \"",month,day+1);
+    switch(month){
+      case "jan": System.out.printf(jan[day].toString()); break;
+      case "feb": System.out.printf(feb[day].toString()); break;
+      case "mar": System.out.printf(mar[day].toString()); break;
+      case "apr": System.out.printf(apr[day].toString()); break;
+      case "may": System.out.printf(may[day].toString()); break;
+      case "jun": System.out.printf(jun[day].toString()); break;
+      case "jul": System.out.printf(jul[day].toString()); break;
+      case "aug": System.out.printf(aug[day].toString()); break;
+      case "sep": System.out.printf(sep[day].toString()); break;
+      case "oct": System.out.printf(oct[day].toString()); break;
+      case "nov": System.out.printf(nov[day].toString()); break;
+      case "dec": System.out.printf(dec[day].toString()); break;
+    }
+    System.out.printf("\"");
+
+  }
+//This is my code for putting a note or an agenda on the calendar for a given day.
+  public static void addAgenda(){
+    System.out.println("To add an agenda please enter a date with the format: first three letters of month [space] day. ");
+    System.out.println("For example March 24th should be written mar 24");
+    Scanner console = new Scanner(System.in);
+    String month = console.next();
     int day = console.nextInt()-1;
     System.out.println("Great! Now please enter the agenda/notes you would like to make");
   //For some reason the program didn't wait for a new input here, and just ended the program, so I
   //Made a temporary fix by creating a new Scanner. Not sure what the original problem was though.
   Scanner input = new Scanner(System.in);
     String agenda = input.nextLine();
-    dates[month][day] = agenda;
-    System.out.println(Arrays.deepToString(dates));
-  //So up to this point we can put an agenda/note into the two dimensional array which represents every
-  //day of the year, and then print out the entire array, with all 365 elements coming up null except
-  //for the one we just changed, but I don't know how to make it print just one date's info, and I want
-  //to make it so it will keep repeating this process until the user types something like "end" in.
+    switch(month){
+      case "jan": jan[day]=agenda; System.out.printf("%nYou have added the agenda item \""+jan[day].toString()+ "\" to jan "+(day+1)); break;
+      case "feb": feb[day]=agenda; System.out.printf("%nYou have added the agenda item \""+feb[day].toString()+ "\" to feb "+(day+1)); break;
+      case "mar": mar[day]=agenda; System.out.printf("%nYou have added the agenda item \""+mar[day].toString()+ "\" to mar "+(day+1)); break;
+      case "apr": apr[day]=agenda; System.out.printf("%nYou have added the agenda item \""+apr[day].toString()+ "\" to apr "+(day+1)); break;
+      case "may": may[day]=agenda; System.out.printf("%nYou have added the agenda item \""+may[day].toString()+ "\" to may "+(day+1)); break;
+      case "jun": jun[day]=agenda; System.out.printf("%nYou have added the agenda item \""+jun[day].toString()+ "\" to jun "+(day+1)); break;
+      case "jul": jul[day]=agenda; System.out.printf("%nYou have added the agenda item \""+jul[day].toString()+ "\" to jul "+(day+1)); break;
+      case "aug": aug[day]=agenda; System.out.printf("%nYou have added the agenda item \""+aug[day].toString()+ "\" to aug "+(day+1)); break;
+      case "sep": sep[day]=agenda; System.out.printf("%nYou have added the agenda item \""+sep[day].toString()+ "\" to sep "+(day+1)); break;
+      case "oct": oct[day]=agenda; System.out.printf("%nYou have added the agenda item \""+oct[day].toString()+ "\" to oct "+(day+1)); break;
+      case "nov": nov[day]=agenda; System.out.printf("%nYou have added the agenda item \""+nov[day].toString()+ "\" to nov "+(day+1)); break;
+      case "dec": dec[day]=agenda; System.out.printf("%nYou have added the agenda item \""+dec[day].toString()+ "\" to dec "+(day+1)); break;
+    }
   }
 }
