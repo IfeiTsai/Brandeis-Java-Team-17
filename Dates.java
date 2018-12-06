1fcd7cf4e5e0dbde5bdf5fac4a1420c709170ef8
IfeiTsai <44784069+IfeiTsai@users.noreply.github.com>
2018-12-06 16:08:13 -0500

    Update Dates.java

--- Dates.java
+++ Dates.java
@@ -2,6 +2,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.*;
 
+/*major concerns: This will all dissapear once the program ends, we need to find a way to save information to a file
+and retrieve it. Additionally this currently can only save one line of input for the agenda at a time, and it's very
+vulnerable to improperly formatted/mistyped inputs from the user. More of a minimal viable product than an actual
+finished project. */
 public class Dates{
   //These are all static arrays for each month containing a block for each day in the respective month.
   static String[] jan = new String[31];
@@ -23,16 +27,18 @@ public class Dates{
     int exit = 0; // if this variable becomes 1 then the loop will end and the program will terminate.
     while(exit !=1){
 //This is the main menu which allows the user to check their agenda for a given day, or add an agenda to another day.
-      System.out.printf("%n%nMAIN MENU:%n Please enter the number which corresponds to your desired option.%n");
-      System.out.printf("1 add an agenda for a day%n2 check an agenda for a day%n3 exit program%n%n");
-      Scanner console = new Scanner(System.in);
-      int option = console.nextInt();
-      switch(option){
-        case 1: addAgenda(); break;
-        case 2: checkAgenda(); break;
-        case 3: System.out.println("goodbye"); exit = 1; break;
-      }
-      //This is a thread to simulate the completition of a task. 
+  System.out.printf("%n%nMAIN MENU:%n Please enter the number which corresponds to your desired option.%n");
+  System.out.printf("1 add an agenda for a new day (or replace existing agenda of that day)%n2 add additional agenda for a day%n3 check an agenda for a day%n4 exit program%n%n");
+  Scanner console = new Scanner(System.in);
+  int option = console.nextInt();
+  switch(option){
+    case 1: addAgenda(); break;
+    case 2: addAdditionalAgenda();break;
+    case 3: checkAgenda(); break;
+    case 4: System.out.println("goodbye"); exit = 1; break;
+    default: System.out.println("Error. Please reenter");exit=0;
+  }
+      //This is a thread to simulate the completition of a task.
       obj = new Dates();
       Timer timer = new Timer();
       TimerTask task = new ReminderTask();
@@ -44,9 +50,9 @@ public class Dates{
         System.out.println("Task completed: " + task.cancel());
         timer.cancel();
 
-
     }
   }
+
   public static void checkAgenda(){
     System.out.printf("Please enter the date you would like to access with the format: first three letters of month [space] day.%n");
     Scanner console = new Scanner(System.in);
@@ -78,10 +84,10 @@ public class Dates{
     String month = console.next();
     int day = console.nextInt()-1;
     System.out.println("Great! Now please enter the agenda/notes you would like to make");
-    Scanner input = new Scanner(System.in);
+  //For some reason the program didn't wait for a new input here, and just ended the program, so I
+  //Made a temporary fix by creating a new Scanner. Not sure what the original problem was though.
+  Scanner input = new Scanner(System.in);
     String agenda = input.nextLine();
-
-
     switch(month){
       case "jan": jan[day]=agenda; System.out.printf("%nYou have added the agenda item \""+jan[day].toString()+ "\" to jan "+(day+1)); break;
       case "feb": feb[day]=agenda; System.out.printf("%nYou have added the agenda item \""+feb[day].toString()+ "\" to feb "+(day+1)); break;
@@ -95,6 +101,34 @@ public class Dates{
       case "oct": oct[day]=agenda; System.out.printf("%nYou have added the agenda item \""+oct[day].toString()+ "\" to oct "+(day+1)); break;
       case "nov": nov[day]=agenda; System.out.printf("%nYou have added the agenda item \""+nov[day].toString()+ "\" to nov "+(day+1)); break;
       case "dec": dec[day]=agenda; System.out.printf("%nYou have added the agenda item \""+dec[day].toString()+ "\" to dec "+(day+1)); break;
+      default:System.out.println("error");
+    }
+  }
+
+  // putting a note or an agenda on the calendar for a day that has existing notes/agenda.
+  public static void addAdditionalAgenda(){
+    System.out.println("To add an agenda please enter a date with the format: first three letters of month [space] day. ");
+    System.out.println("For example March 24th should be written mar 24");
+    Scanner console = new Scanner(System.in);
+    String month = console.next();
+    int day = console.nextInt()-1;
+    System.out.println("Great! Now please enter the agenda/notes you would like to make");
+    Scanner input = new Scanner(System.in);
+    String agenda = ";"+ input.nextLine();
+    switch(month){
+    case "jan": jan[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to jan %d",agenda,day+1); break;
+    case "feb": feb[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to feb %d",agenda,day+1); break;
+    case "mar": mar[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to mar %d",agenda,day+1); break;
+    case "apr": apr[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to apr %d",agenda,day+1); break;
+    case "may": may[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to may %d",agenda,day+1); break;
+    case "jun": jun[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to jun %d",agenda,day+1); break;
+    case "jul": jul[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to jul %d",agenda,day+1); break;
+    case "aug": aug[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to aug %d",agenda,day+1); break;
+    case "sep": sep[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to sept %d",agenda,day+1); break;
+    case "oct": oct[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to oct %d",agenda,day+1); break;
+    case "nov": nov[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to nov %d",agenda,day+1); break;
+    case "dec": dec[day]+=agenda; System.out.printf("%nYou have added the agenda item %s to dec %d",agenda,day+1); break;
+    default:System.out.println("error");
     }
   }
 }
